720f2cd478fc5b37e30adb667bf09f1344343ee6
Paul Mathieu <paul@ponteilla.net>
2011-06-25 04:10:46 +0000

    Added a config file (in fact, a class)
     - also separated server from FW, put them in different files
     - maybe having the fw in the form of a subdir would be good...
    
     fixes #4

---
+++ config.py
@@ -0,0 +1,13 @@
+class MRConfig:
+
+    listen_address = "localhost"
+    listen_port = 1337
+
+    db_file = "world.sav"
+    op_password = "lol"
+    op_command_prefix = '@'
+
+    client_class = "MRClient"
+    db_class = "MRDB"
+
+
