b34cf262c10ef84c026ac8255033b320e80ab353
Paul Mathieu <paul@ponteilla.net>
2015-01-23 09:21:44 -0800

    Factor out lock business in util.RWLock class
    It is now exception-safe, and more importantly, much prettier!
    
    with lock.r:
        read_stuff()
    
    with lock.w:
        write_stuff()

--- fw/db.py
+++ fw/db.py
@@ -1,5 +1,4 @@
 import pickle
-import threading
 
 from . import util
 from .interface import BaseObject
@@ -11,67 +10,32 @@ class Database:
         self.objects = {}
         self.ids = {}  # use a reverse map
         self.last_id = 0
-        self.readers = 0
-        self.writers = 0
-        self.lock = threading.Lock()
-        self.r_cv = threading.Condition(self.lock)
-        self.w_cv = threading.Condition(self.lock)
-
-    def acquire_r(self):
-        with self.lock:
-            while self.readers < 0 or self.writers:
-                self.r_cv.wait()
-            self.readers += 1
-
-    def acquire_w(self):
-        with self.lock:
-            while self.readers != 0:
-                self.writers += 1
-                self.w_cv.wait()
-                self.writers -= 1
-            self.readers = -1
-
-    def release(self):
-        with self.lock:
-            if self.readers < 0:
-                self.readers = 0
-            else:
-                self.readers -= 1
-            if self.writers and self.readers == 0:
-                self.w_cv.notify()
-            elif self.writers == 0:
-                self.r_cv.notifyAll()
+        self.lock = util.RWLock()
 
     def add(self, obj):
         if not isinstance(obj, BaseObject):
             raise Exception("Trying to add random trash to the DB!")
-        self.acquire_w()
-        self.objects[self.last_id] = obj
-        self.ids[obj] = self.last_id
-        self.last_id += 1
-        self.release()
+        with self.lock.r:
+            self.objects[self.last_id] = obj
+            self.ids[obj] = self.last_id
+            self.last_id += 1
 
     def remove(self, obj):
-        self.acquire_w()
-        if type(obj) is int:
-            del self.ids[self.objects[obj]]
-            del self.objects[obj]
-        else:
-            del self.objects[self.ids[obj]]
-            del self.ids[obj]
-        self.release()
+        with self.lock.w:
+            if type(obj) is int:
+                del self.ids[self.objects[obj]]
+                del self.objects[obj]
+            else:
+                del self.objects[self.ids[obj]]
+                del self.ids[obj]
 
     def get(self, obj_id):
-        self.acquire_r()
-        obj = self.objects.get(obj_id, None)
-        self.release()
-        return obj
+        with self.lock.r:
+           return self.objects.get(obj_id, None)
 
     def get_id(self, obj):
-        self.acquire_r()
-        obj_id = self.ids[obj]
-        self.release()
-        return obj_id
+        with self.lock.r:
+            return self.ids[obj]
 
     def load(self, db_file):
         with open(db_file, 'rb') as f:
@@ -82,26 +46,22 @@ class Database:
                     self.ids[v] = k
 
     def dump(self, db_file):
-        self.acquire_r()
-        with open(db_file, 'wb') as f:
-            pickle.dump(self.objects, f)
-        self.release()
+        with self.lock.r:
+            with open(db_file, 'wb') as f:
+                pickle.dump(self.objects, f)
 
     def search(self, name, type=BaseObject):
         found = []
-        self.acquire_r()
-        for thing in self.objects.values():
-            if util.match_name(name, thing.name):
-                if isinstance(thing, type):
-                    found.append(thing)
-        self.release()
+        with self.lock.r:
+            for thing in self.objects.values():
+                if util.match_name(name, thing.name):
+                    if isinstance(thing, type):
+                        found.append(thing)
         return found
 
     def list_all(self, type):
-        self.acquire_r()
-        obj_list = self.search("", type)
-        self.release()
-        return obj_list
+        with self.lock.r:
+            return self.search("", type)
 
 # global instance
 db = Database()
