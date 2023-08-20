3e1d5bca9d93766807cd2856e5e68878e788c370
Paul Mathieu <paul@ponteilla.net>
2023-08-20 20:17:54 +0000

    ws: only bind to 127.0.0.1 for now

--- client/index.js
+++ client/index.js
@@ -4,9 +4,10 @@ const websocket = require('ws');
 const mr_host = '127.0.0.1';
 const mr_port = 1337;
 
+const ws_host = '127.0.0.1';
 const ws_port = 1338;
 
-const wss = new websocket.Server({port: ws_port});
+const wss = new websocket.Server({port: ws_port, host: ws_host});
 wss.on('connection', ws => {
     const socket = net.createConnection(mr_port, mr_host);
 
