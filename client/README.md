92e0efb55ea058ca2fdfe91aec7bd284dd9a1776
Paul Mathieu <paul@ponteilla.net>
2023-08-20 22:10:11 +0200

    Add web client

---
+++ client/README.md
@@ -0,0 +1,10 @@
+How do I do it??
+----------------
+
+```
+yarn install # once
+node index.js
+```
+
+Launch the game.
+Then open `index.html` in a browser. Click connect.
---
+++ client/index.js
@@ -0,0 +1,27 @@
+const net = require('net');
+const websocket = require('ws');
+
+const mr_host = '127.0.0.1';
+const mr_port = 1337;
+
+const ws_port = 1338;
+
+const wss = new websocket.Server({port: ws_port});
+wss.on('connection', ws => {
+    const socket = net.createConnection(mr_port, mr_host);
+
+    socket.on('data', data => {
+        ws.send(data);
+    });
+
+    socket.on('close', () => {
+        ws.close();
+    });
+
+    ws.on('message', data => {
+        socket.write(data);
+    });
+    ws.on('close', () => {
+        socket.end();
+    });
+});
